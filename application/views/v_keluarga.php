624f558c7e31c07a599dfeff8bb81a9f28ecbaf9
Arisandy Maulana Figo 86 <arisandy_maulana_26rpl@student.smktelkom-mlg.sch.id>
2019-01-23 12:44:12 +0700

    lanjutan ci selesai

---
+++ application/views/v_keluarga.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title></title>
+<style>
+    h1 {
+        text-align: center;
+        font-size: 70px;
+        font-family: chiller;
+        padding:50px 0px 20px 0px;
+        text-shadow: 2px 2px 2px black;
+
+      }
+</style>
+<marquee behavior="alternate" direction="up" scrollamount="3" scrolldelay="65" height="80" style="text-align" filter:wave></div>
+    <h1>My Family</h1></marquee>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/family.jpg')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>My Father</td>
+                                <td>:</td>
+                                <td>Ichsan Rosadi</td>
+                            </tr>
+                            <tr>
+                                <td>My Mother</td>
+                                <td>:</td>
+                                <td>Ely Hari Sulistyowati</td>
+                            </tr>
+                            <tr>
+                                <td>Address</td>
+                                <td>:</td>
+                                <td>Jalan Penataran no. 24, Banyuwangi</td>
+                            </tr>
+                            <tr>
+                                <td>Job</td>
+                                <td>:</td>
+                                <td>Wiraswasta</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+</head>
+
+</html>
